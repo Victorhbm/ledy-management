8c1ede5524da3ac82e0c30a62149d473aea60355
Victorhbm <victor.hugofla@hotmail.com>
2022-12-25 18:06:52 +0000

    feat: create the App

---
+++ backend/src/app.ts
@@ -0,0 +1,32 @@
+import * as express from 'express';
+import * as cors from 'cors';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config():void {
+    const acessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Acess-Control-Allow-Origin', '*');
+      res.header('Acess-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT, PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    }
+
+    this.app.use(acessControl);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT);
+    console.log(`Server is running on PORT: ${PORT}`);
+  }
+
+  private routes():void {
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
+}

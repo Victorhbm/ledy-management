529b867ce64c5d6ccfbe1d8c44c1315ba53f0661
Victorhbm <victor.hugofla@hotmail.com>
2023-02-04 15:25:52 +0000

    feat: create Category migration and model

---
+++ backend/src/database/models/Category.ts
@@ -0,0 +1,28 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Subcategory from './Subcategory';
+
+class Category extends Model {
+  declare id: number;
+  declare name: string;
+}
+
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: DataTypes.STRING,
+}, {
+  sequelize: db,
+  tableName: 'Categories',
+  timestamps: false,
+});
+
+Category.hasMany(Subcategory, {
+  foreignKey: 'categoryID'
+});
+Subcategory.belongsTo(Category);
+
+export default Category;

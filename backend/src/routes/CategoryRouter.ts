c8fd4234f00882967ed588a1e086076a867b08fb
Victorhbm <victor.hugofla@hotmail.com>
2023-02-04 15:28:11 +0000

    feat: create Category router

---
+++ backend/src/routes/CategoryRouter.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import CategoryController from "../controllers/CategoryController";
+
+const router = Router();
+
+router.post('/',
+  CategoryController.create
+);
+
+router.get('/',
+  CategoryController.getAll
+);
+
+router.get('/:id',
+  CategoryController.getById
+);
+
+router.put('/:id',
+  CategoryController.update
+);
+
+router.delete('/:id',
+  CategoryController.delete
+);
+
+export default router;

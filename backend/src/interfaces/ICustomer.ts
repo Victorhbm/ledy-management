7de3ef515efea60268bbdce2103c27b652b3abd1
Victorhbm <victor.hugofla@hotmail.com>
2023-02-13 20:10:10 +0000

    feat: creratet ICustomer interface

---
+++ backend/src/interfaces/ICustomer.ts
@@ -0,0 +1,11 @@
+export interface ICustomer {
+  name: string;
+  email?: string;
+  CPF?: string;
+  phoneNumber?: string;
+  state?: string;
+  city?: string;
+  neighborhood?: string;
+  address?: string;
+  birthdate?: Date;
+}
